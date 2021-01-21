4d5493f3eb708887b908b20052bd327c21581f3b
MucaBordini <mucabordini@gmail.com>
2021-01-21 10:11:04 -0300

    Definindo e extraindo parâmetros de url (query)

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -9,11 +11,26 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursoService: CursosService) { }
+  proximaPagina(): void {
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': ++this.pagina}})
+  }
+
+  constructor(private cursoService: CursosService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.cursos = this.cursoService.getCursos();
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
