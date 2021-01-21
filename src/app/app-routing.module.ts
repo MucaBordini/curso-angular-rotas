b249c5966eddda5979deba9b29facf1a2d67ba02
MucaBordini <mucabordini@gmail.com>
2021-01-21 10:41:48 -0300

    Criando um módulo de funcionalidade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from "./login/login.component";
 import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
-const APP_ROUTES: Routes = [
+const appRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'cursos', component: CursosComponent },
@@ -19,7 +19,7 @@ const APP_ROUTES: Routes = [
 ];
 
 @NgModule({
-    imports: [ RouterModule.forRoot(APP_ROUTES) ],
+    imports: [ RouterModule.forRoot(appRoutes) ],
     exports: [RouterModule],
   declarations: []
 })
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursosComponent } from "./cursos.component";
+import { CursosService } from "./cursos.service";
+
+@NgModule({
+    imports: [
+        CommonModule,
+    ],
+    exports: [],
+    declarations: [
+        CursosComponent,
+        CursoNaoEncontradoComponent,
+        CursoDetalheComponent
+    ],
+    providers: [CursosService],
+})
+export class CursosModule { }
