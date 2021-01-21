4baeb04e55e152e82e4ea10a7e18f1d2572cb02e
MucaBordini <mucabordini@gmail.com>
2021-01-21 09:59:28 -0300

    Rotas Imperativas: Redirecionamento via código

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from "./login/login.component";
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 const APP_ROUTES: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'cursos', component: CursosComponent },
   { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'notFound', component: CursoNaoEncontradoComponent },
   { path: 'login', component: LoginComponent },
 ];
 
